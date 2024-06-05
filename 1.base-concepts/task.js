72bd4ef7f9a641c9b71cdbbcfa95b152dffd1472
ParfenovPetr8 <parfenovpetr@gmail.com>
2024-06-06 01:02:23 +0300

    Исправлены имена переменных и место объявления, добавлено округление числа

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,31 +1,32 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = Math.pow(b, 2) - 4 * a * c;
-  let sq;// один корень при условии d=0
-  let sq1;// 1-ый корень при d>0
-  let sq2;// 2-ой корень при d>0
-
-  if (d === 0) {
-    sq = -b / (2 * a);
-    arr.push(sq);
-  } else if (d > 0) {
-    sq1 = (-b + Math.sqrt(d)) / (2 * a);
-    sq2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(sq1, sq2);
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  
+  if (discriminant === 0) {
+    let squareRoot = -b / (2 * a);
+    arr.push(squareRoot);
+  } else if (discriminant > 0) {
+    let squareRootFirst = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let squareRootSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(squareRootFirst, squareRootSecond);
   }
 
-  return arr; // возвращает пустой маасив и заканчивает
+  return arr;
 }
 
-solveEquation(2, 3, 4);
+solveEquation();
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent / (12 * 100);// процентная ставка в месяц (от 0 до 1)
-  let n = 12;// 12 месяцев в году;
-  let S = amount - contribution ;//тело кредита = сумма кредита - первоначальный взнос;    
-  let M = S * (P + (P / ((Math.pow((1 + P), n) - 1)))// Ежемесячная оплата
-  Let E = M * countMonths;// общая сумма, которую придётся заплатить клиенту
+  let interestRateMonth = percent / (12 * 100);
+  let P = interestRateMonth; 
+  let n = countMonths; 
+  let loanBody = amount - contribution;
+  let S = loanBody; 
+  let monthlyPayment = S * (P + P / (Math.pow(1 + P, n) - 1));
+  let totalAmount = monthlyPayment * countMonths;
+  return Number(totalAmount.toFixed(2));
 }
 
-calculateTotalMortgage(10, 0, 50000, 12)
+calculateTotalMortgage();
