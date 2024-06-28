123863eca602f74bd6b1682365f6d6192a4b136c
ParfenovPetr8 <parfenovpetr@gmail.com>
2024-06-28 20:06:42 +0300

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,48 @@
-function Student(name, gender, age) {
-  
-}
+function Student(name, gender, age) { // Функция-конструктор Student
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.subject = null; // Предмет не установлен изначально
+    this.marks = []; // Пустой массив для хранения оценок
+};
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function (subjectName) { // Метод для установки предмета
+    this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+//Student.prototype.Marks = function (...marks) { // Метод для добавления оценок
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.hasOwnProperty('marks')) { // Проверка, что студент не исключен (не удалено свойство marks)
+        //this.marks.push(...marks);
+        this.marks.push(...marksToAdd);
+      } else {
+        console.log(`${this.name} исключен из учебного процесса.`);
+      }
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function () { // Метод для вычисления среднего значения оценок
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+      }
+      const sum = this.marks.reduce((total, mark) => total + mark, 0);
+      return sum / this.marks.length;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function (reason) { // Метод для исключения студента
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
+
+/* Пример использования
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);*/
